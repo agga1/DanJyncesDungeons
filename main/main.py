5f91ba5f6afdcf68b917864ad50cbf88de04e168
sheldak <sheldak426@gmail.com>
2019-08-25 19:48:50 +0200

    border and character added

--- main/main.py
+++ main/main.py
@@ -2,6 +2,8 @@ import pygame
 
 pygame.init()
 
+# ----- VARIABLES -----
+
 # SCREEN
 screen_width = 600
 screen_height = 600
@@ -11,13 +13,36 @@ pygame.display.set_caption("Dan Jynce's Dungeons")
 # TIME
 clock = pygame.time.Clock()
 
+# IMAGES
+character_image = pygame.image.load("../resources/main_character.png")
+item_image = pygame.image.load("../resources/item.png")
+enemy_image = pygame.image.load("../resources/enemy.png")
+terrain_border_image = pygame.image.load("../resources/terrain_border.png")
+
+# POINTS
+character_start_point = (300, 300)
+
+
+# ----- FUNCTIONS -----
+def wall_display():
+    for i in range(0, 12):
+        screen.blit(terrain_border_image, (50 * i, 0))
+        screen.blit(terrain_border_image, (50 * i, 550))
+
+    for i in range(0, 11):
+        screen.blit(terrain_border_image, (0, 50 + 50 * i))
+        screen.blit(terrain_border_image, (550, 50 + 50 * i))
+
+
+# ----- MAIN LOOP -----
 while True:
 
     clock.tick(90)
 
+    # TERRAIN
     screen.fill((255, 255, 255))
-
-    pygame.draw.line(screen, (255, 0, 255), (20, 30), (200, 300), 5)
+    screen.blit(character_image, character_start_point)
+    wall_display()
 
     # QUIT GAME
     for e in pygame.event.get():
