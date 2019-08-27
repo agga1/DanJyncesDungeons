14b5a32bfbd0d5efb1ba3c44e2521389a16c5e63
agga1 <agud100@gmail.com>
2019-08-27 21:10:17 +0200

    character walk db first drafts, bats

--- db/user_database.py
+++ db/user_database.py
@@ -2,20 +2,23 @@ import sqlite3
 
 db = sqlite3.connect('data/test_db')
 CREATE_TABLE = '''
-    CREATE TABLE stats(id INTEGER PRIMARY KEY, money INTEGER, health INTEGER, inventory TEXT)
+    CREATE TABLE stats(id INTEGER AUTOINCREMENT PRIMARY KEY, money INTEGER, health INTEGER, inventory TEXT)
 '''
 cursor = db.cursor()
 cursor.execute(CREATE_TABLE)
 db.commit()
 
+
 def insert(): # create new row with new game instance
     cursor.execute('''INSERT INTO stats(money, health, inventory)
-    VALUES(0, 0, "")
-                
-    ''')
+    VALUES(?, ?, ?)''', (0, 0, ""))
+    db.commit()
+    return cursor.lastrowid
 
-# def update_money(row_id, money):
 
+def update_money(row_id, money):
+    cursor.execute('''UPDATE stats SET money = ? WHERE id = ? ''', (money, row_id))
+    db.commit()
 
 
 
