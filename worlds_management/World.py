58c9144f15c055ed4e941b3924f67ea33007b760
Samuel <sheldak426@gmail.com>
2019-09-11 14:27:33 +0200

    changes in curr_room variable in World class

--- worlds_management/World.py
+++ worlds_management/World.py
@@ -7,7 +7,7 @@ from sprites_management.sprites_manager import create_enemy
 from management_and_config.configurations import *
 
 
-def extract_room_position(room):     # example: "room_1_12.txt", we need pos_x = 1 and pos_y = 12
+def extract_room_position(room):  # example: "room_1_12.txt", we need pos_x = 1 and pos_y = 12
     room = room.split("_")
     pos_x = int(room[1])
 
@@ -24,23 +24,23 @@ class World:
         # world configurations
         self._size = [0, 0]
         self._rooms = []
-        self._start_room = [0, 0]
+        self._start_room_pos = [0, 0]
 
         # reading world configurations from a world file
         world_file = open(self._path + "/world.txt", "r")
 
         world_size = world_file.readline().split()
-        self._size[0] = int(world_size[0])   # number of elements from left to right
-        self._size[1] = int(world_size[1])   # number of elements from top to bottom
+        self._size[0] = int(world_size[0])  # number of elements from left to right
+        self._size[1] = int(world_size[1])  # number of elements from top to bottom
 
-        for i in range(0, self._size[1]):    # making 2d array
+        for i in range(0, self._size[1]):  # making 2d array
             self._rooms.append([None] * self._size[0])
 
-        for i in range(0, self._size[1]):    # checking rooms in the world (now just checking which is the start one)
+        for i in range(0, self._size[1]):  # checking rooms in the world (now just checking which is the start one)
             curr_line = world_file.readline().split()
             for j in range(0, self._size[0]):
-                if int(curr_line[j]) == 1:      # no room == 0, start room == 1, other rooms == 2
-                    self._start_room = [j, i]
+                if int(curr_line[j]) == 1:  # no room == 0, start room == 1, other rooms == 2
+                    self._start_room_pos = [j, i]
         world_file.close()
 
         # active_enemies (array read from db w/ values enemy_id: 0/1 (0- enemy dead),
@@ -49,43 +49,47 @@ class World:
         self._active_enemies = load_active_enemies(db)
 
         # start room
-        self._curr_room = load_curr_room(db)    # position of the room in world map TODO change to curr_room_pos and make self.curr_room as object
-        if self._curr_room is None:  # when new game opens, curr_room in db is not initialized!
-            self._curr_room = self._start_room
+        self._curr_room_pos = load_curr_room(db)  # position of the room in world map
+        if self._curr_room_pos is None:  # when new game opens, curr_room in db is not initialized!
+            self._curr_room_pos = self._start_room_pos
 
-    def load_world(self):   # called when starting game or changing world
+        # current room object
+        self._curr_room = None
+
+    def load_world(self):  # called when starting game or changing world
         rooms = os.listdir(self._path)
-        enemy_id = 0    # to give every enemy unique id
+        enemy_id = 0  # to give every enemy unique id
 
         # reading rooms' configuration files
         for room in rooms:
             if room != "world.txt":
                 position = extract_room_position(room)
 
-                room_file = open(self._path + "/" + room)    # reading specification of the room
+                room_file = open(self._path + "/" + room)  # reading specification of the room
 
-                room_size = room_file.readline().split()    # size of the room [from left to right, from top to bottom]
+                room_size = room_file.readline().split()  # size of the room [from left to right, from top to bottom]
                 room_size = [int(room_size[0]), int(room_size[1])]
 
-                room_type = room_file.readline().split()    # places with doors ("top", "bottom", "left", "right")
+                room_type = room_file.readline().split()  # places with doors ("top", "bottom", "left", "right")
 
-                enemies_number = int(room_file.readline().split()[0])   # number of enemies in the room
+                enemies_number = int(room_file.readline().split()[0])  # number of enemies in the room
 
-                enemies = []    # list of enemies in the room
+                enemies = []  # list of enemies in the room
 
                 for enemy in range(0, enemies_number):
                     if len(self._active_enemies) > enemy_id and not self._active_enemies[enemy_id]:
                         enemy_id += 1
                         continue
 
-                    if len(self._active_enemies) <= enemy_id: # the first time game opens, active_enemies is empty. default is 1 - enemy active
+                    # the first time game opens, active_enemies is empty. default is 1 - enemy active
+                    if len(self._active_enemies) <= enemy_id:
                         self._active_enemies.append(1)
 
-                    curr_enemy_vars = room_file.readline().split()  # readiang variables connected with the enemy
+                    curr_enemy_vars = room_file.readline().split()  # reading variables connected with the enemy
 
-                    enemy_type = curr_enemy_vars[0]     # type (name) of the enemy
+                    enemy_type = curr_enemy_vars[0]  # type (name) of the enemy
 
-                    enemy_start_point = [int(curr_enemy_vars[1]),   # point where is the enemy at the beginning
+                    enemy_start_point = [int(curr_enemy_vars[1]),  # point where is the enemy at the beginning
                                          int(curr_enemy_vars[2])]
                     enemies.append(create_enemy(enemy_id, enemy_type, enemy_start_point))  # creating and adding object
 
@@ -93,7 +97,10 @@ class World:
 
                 room_file.close()
 
-                self._rooms[position[0]][position[1]] = Room(room_size, room_type, enemies)   # adding room
+                self._rooms[position[0]][position[1]] = Room(room_size, room_type, enemies)  # adding room
+
+        # start room
+        self._curr_room = self._rooms[self._curr_room_pos[0]][self._curr_room_pos[1]]
 
     def check_room(self, main_character):
         # checking if we are changing room
@@ -101,38 +108,47 @@ class World:
 
         if main_character_pos[1] < 0:
             self.change_room_and_save("top", main_character)
-        elif main_character_pos[1] > self._rooms[self._curr_room[0]][self._curr_room[1]].size[1]*50 - sprite_size[1]:
+        elif main_character_pos[1] > self._curr_room.size[1] * 50 - sprite_size[1]:
             self.change_room_and_save("bottom", main_character)
         elif main_character_pos[0] < 0:
             self.change_room_and_save("left", main_character)
-        elif main_character_pos[0] > self._rooms[self._curr_room[0]][self._curr_room[1]].size[0]*50 - sprite_size[0]:
+        elif main_character_pos[0] > self._curr_room.size[0] * 50 - sprite_size[0]:
             self.change_room_and_save("right", main_character)
 
-    def change_room_and_save(self, direction, main_character): # TODO: maybe save only in checkpoints? if killed -> reload last checkpoint
-        # changing room
+    def change_room_and_save(self, direction, main_character):
+        # TODO: maybe save only in checkpoints? if killed -> reload last checkpoint
         # save game
         self._active_enemies = self.curr_room.update_active_enemies(self._active_enemies)
         save_active_enemies(self._active_enemies, self._db)
         save_character(main_character, self._db)
-        save_curr_room(self._curr_room, self._db)
+        save_curr_room(self._curr_room_pos, self._db)
 
+        # changing curr_room_pos variable
         if direction == "top":
-            if self._curr_room[1] > 0 and self._rooms[self._curr_room[0]][self._curr_room[1] - 1]:
-                self._curr_room[1] -= 1
+            if self._curr_room_pos[1] > 0 and self._rooms[self._curr_room_pos[0]][self._curr_room_pos[1] - 1]:
+                self._curr_room_pos[1] -= 1
+
         elif direction == "bottom":
-            if self._curr_room[1] < self._size[1] - 1 and self._rooms[self._curr_room[0]][self._curr_room[1] + 1]:
-                self._curr_room[1] += 1
+            if self._curr_room_pos[1] < self._size[1] - 1 and \
+                    self._rooms[self._curr_room_pos[0]][self._curr_room_pos[1] + 1]:
+                self._curr_room_pos[1] += 1
+
         elif direction == "left":
-            if self._curr_room[0] > 0 and self._rooms[self._curr_room[0] - 1][self._curr_room[1]]:
-                self._curr_room[0] -= 1
+            if self._curr_room_pos[0] > 0 and self._rooms[self._curr_room_pos[0] - 1][self._curr_room_pos[1]]:
+                self._curr_room_pos[0] -= 1
+
         elif direction == "right":
-            if self._curr_room[0] < self._size[0] - 1 and self._rooms[self._curr_room[0] + 1][self._curr_room[1]]:
-                self._curr_room[0] += 1
+            if self._curr_room_pos[0] < self._size[0] - 1 and \
+                    self._rooms[self._curr_room_pos[0] + 1][self._curr_room_pos[1]]:
+                self._curr_room_pos[0] += 1
+
+        # changing current room variable
+        self._curr_room = self._rooms[self._curr_room_pos[0]][self._curr_room_pos[1]]
 
         # setting position of the character
-        main_character.set_position(direction, self._rooms[self._curr_room[0]][self._curr_room[1]].size)
+        main_character.set_position(direction, self._curr_room.size)
 
     @property
     def curr_room(self):
         """ current room (object) """
-        return self._rooms[self._curr_room[0]][self._curr_room[1]]
+        return self._curr_room
